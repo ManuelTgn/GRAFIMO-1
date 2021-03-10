30cf2ffe2bb936ea53224a74166570395b36633e
ManuelTgn <51021763+ManuelTgn@users.noreply.github.com>
2021-03-10 10:06:33 +0100

    new help format

--- src/grafimo/GRAFIMOArgumentParser.py
+++ src/grafimo/GRAFIMOArgumentParser.py
@@ -81,15 +81,35 @@ class GRAFIMOArgumentParser(ArgumentParser):
         kwargs['usage'] = kwargs['usage'].replace("{version}", __version__)
         super().__init__(*args, **kwargs)
 
-
+    
     def error(
-        self, 
+        self,
         msg: str
     ) -> None:
         """Print the given message when argparse raise an error.
 
         At the bottom is also printed a new message telling the 
-        user how to call the halp
+        user how to call the help.
+
+        Parameters
+        ----------
+        msg : str
+            Message which will be shown when raising an error
+        """
+
+        errmsg = "{}: ERROR: {}.\n\nRun \"grafimo --help\" to see usage\n\n"
+        sys.stderr.write(errmsg.format(self.prog, msg))
+        sys.exit(2)
+
+
+    def error_noargs(
+        self 
+    ) -> None:
+        """Print the given message when argparse raise an error.
+        
+        This error message s raised only when no arguments is given when
+        calling grafimo from command line. Prints the general help 
+        message.
 
         Parameters
         ----------
@@ -97,8 +117,8 @@ class GRAFIMOArgumentParser(ArgumentParser):
             Message which will be shown when raising an error
         """
         
-        sys.stderr.write("Run 'grafimo --help' to see the usage\n")
-        self.exit(2, "\n{0}: ERROR: {1}\n".format(self.prog, msg))
+        self.print_help()
+        sys.exit(2)
 
 # end of GRAFIMOArgumentParser
 
